0b5ea545191fe9f3229f0bd563730afabafcc0ab
arunlalpp <arunlalpp@gmail.com>
2022-05-21 22:00:45 +0530

    array

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -29,7 +29,7 @@ export default function ImageSlider() {
     return () => {
       pauseSlider();
     };
-  }, []);
+  }, [ ]);
   const startSlider = () => {
     slideInterval = setInterval(() => {
       handleOnNextClick();
